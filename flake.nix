2c6583dd3889fce6f1cb998ea0d78d8fc1aa01e3
mislavzanic <mislavzanic3@gmail.com>
2023-07-21 15:36:34 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -20,6 +20,6 @@
     lib = nixpkgs.lib.extend (mkLib {inherit pkgs inputs;});
   in {
     modules = mapModulesRec ./modules import;
-    modulesArr = mapModulesRec' ./modules import;
+    modulesArr = mapModulesRec' (toString ./modules) import;
   };
 }
