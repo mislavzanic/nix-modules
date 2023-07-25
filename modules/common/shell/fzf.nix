b11826e247b39953eb34ed71583792e45d70ea45
mislavzanic <mislavzanic3@gmail.com>
2023-07-25 16:43:03 +0200

    test

--- modules/common/shell/fzf.nix
+++ modules/common/shell/fzf.nix
@@ -8,13 +8,14 @@
 with lib;
 with lib.my; let
   cfg = config.modules.shell.fzf;
+  cfgType = config.type;
 in {
   options.modules.shell.fzf = {
     enable = mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
-    user.packages = [ pkgs.fzf ];
+    config.modules.${cfgType}.userPackages = [ pkgs.fzf ];
     modules.shell.zsh.rcInit = ''
       source "$(fzf-share)/key-bindings.zsh"
       source "$(fzf-share)/completion.zsh"
