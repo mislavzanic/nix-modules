61e466c433dd91db1496023a3db5af81270485cc
mislavzanic <mislavzanic3@gmail.com>
2023-07-30 23:08:38 +0200

    add picom

--- modules/common/desktop/wm.nix
+++ modules/common/desktop/wm.nix
@@ -8,47 +8,67 @@ let
 in {
   options.modules.desktop.wm = with types; {
     enable = mkBoolOpt false;
+    picom = {
+      enable = mkBoolOpt false;
+    };
   };
 
-  config = mkIf cfg.enable {
-    core = {
-      xserver = {
+  config = mkIf cfg.enable (mkMerge [
+    {
+      core = {
+        xserver = {
+          enable = true;
+          defaultSession = "none+myxmonad";
+          session = {
+            name = "myxmonad";
+            start = ''
+              /usr/bin/env mzanic-xmonad &
+              waitPID=$!
+            '';
+          };
+        };
+        packages = with pkgs; [
+          xmonad-log
+          haskellPackages.mzanic-xmonad
+          haskellPackages.mzanic-xmobar
+          trayer
+          i3lock
+          xss-lock
+          networkmanagerapplet
+          pasystray
+        ];
+
+        fonts = with pkgs; [
+          font-awesome_5
+          nerdfonts
+          cantarell-fonts
+          noto-fonts-emoji
+        ];
+      };
+
+      home.configFile = {
+        "xmobar/xpm".source = "${configDir}/xmobar/xpm";
+        "xmobar/trayer-padding-icon.sh" = {
+          source = "${configDir}/xmobar/trayer-padding-icon.sh";
+          executable = true;
+        };
+        "x11".source = "${configDir}/x11";
+      };
+    }
+    {
+      services.picom = {
         enable = true;
-        defaultSession = "none+myxmonad";
-        session = {
-          name = "myxmonad";
-          start = ''
-            /usr/bin/env mzanic-xmonad &
-            waitPID=$!
-          '';
+        backend = "glx";
+        vSync = true;
+        inactiveOpacity = 0.9;
+        settings = {
+          blur = {
+            method = "gaussian";
+            size = 10;
+            deviation = 5.0;
+          };
         };
       };
-      packages = with pkgs; [
-        xmonad-log
-        haskellPackages.mzanic-xmonad
-        haskellPackages.mzanic-xmobar
-        trayer
-        i3lock
-        xss-lock
-        networkmanagerapplet
-        pasystray
-      ];
-
-      fonts = with pkgs; [
-        font-awesome_5
-        nerdfonts
-        cantarell-fonts
-        noto-fonts-emoji
-      ];
-    };
-
-    home.configFile = {
-      "xmobar/xpm".source = "${configDir}/xmobar/xpm";
-      "xmobar/trayer-padding-icon.sh" = {
-        source = "${configDir}/xmobar/trayer-padding-icon.sh";
-        executable = true;
-      };
-      "x11".source = "${configDir}/x11";
-    };
-  };
+    }
+  ]);
 }
