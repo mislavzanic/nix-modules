8a887ee3f48840dd95f51cf12fce9c881062bf59
mislavzanic <mislavzanic3@gmail.com>
2023-09-01 16:14:21 +0200

    remove xmonad and xmobar stuff into its own flake

--- flake.nix
+++ flake.nix
@@ -55,10 +55,10 @@
     utilModules = nix-utils.modules;
     mkModuleArr = path: mapModulesRec' path import;
     allModules = (mapModulesRec ./modules import);
-    hmModules = [utilModules.common utilModules.home]
+    hmModules = [utilModules.common utilModules.home xmonad-config.module]
                 ++ (mkModuleArr (toString ./modules/common))
                 ++ (mkModuleArr (toString ./modules/systems/home));
-    nixosModules = [utilModules.common utilModules.nixos]
+    nixosModules = [utilModules.common utilModules.nixos xmonad-config.module]
                    ++ (mkModuleArr (toString ./modules/common))
                    ++ (mkModuleArr (toString ./modules/systems/nixos));
 
